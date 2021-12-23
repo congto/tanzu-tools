170bb45e6281dfabf9a38cc8aad57b73b4ac126f
Tô Thành Công <tcvn1985@gmail.com>
2021-12-23 10:18:58 +0700

    Update 01_install-cli.sh

--- scripts/01_install-cli.sh
+++ scripts/01_install-cli.sh
@@ -7,7 +7,7 @@ sudo snap install yq
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' .bashrc
 
 # SSH Key
-ssh-keygen -t rsa -b 4096
+su - congto -c "ssh-keygen -q -b 4096 -t rsa -N '' -f ~/.ssh/id_rsa"
 
 # Kubernetes
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
